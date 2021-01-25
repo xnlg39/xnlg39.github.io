02f7c0dec30cf692cae24442fe8869a1e7d3ecac
Grayson Maguire <63260895+GraysonMaguire@users.noreply.github.com>
2021-01-25 09:57:18 +0000

    started adding content to climate Activism

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $(window).scroll(function () {
         $('.home__nav').toggleClass(
             'home__nav--scrolled',
-            $(this).scrollTop() >= 50
+            $(this).scrollTop() >= 5
         );
         $('.home__nav').toggleClass('home__nav--top', $(this).scrollTop() < 50);
     });
